db7937ff674ef5ec87b9b3c8e02cd303ce584705
Joseph Potashnik <joseph.potashnik@gmail.com>
2025-05-07 17:52:05 +0100

    install cors

--- src/controllers/topicController.ts
+++ src/controllers/topicController.ts
@@ -3,6 +3,7 @@ import { Request, Response } from "express";
 
 export const getTopics = async (req: Request, res: Response): Promise<void> => {
   try {
+    console.log("topic controller entered");
     res.status(200).json({ message: "Hello from topic controller" });
   } catch (error) {
     res.status(500).json({ error: "Internal Server Error" });
--- src/server.ts
+++ src/server.ts
@@ -2,10 +2,16 @@ import dotenv from "dotenv";
 dotenv.config();
 
 import express, { Request, Response } from "express";
+import cors from 'cors';
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(cors({
+  origin: 'http://localhost:3000',
+  credentials: true // if you're sending cookies or Authorization headers
+}));
+
 
 import cardRouter from "./routes/cardRouter";
 import topicRouter from "./routes/topicRouter";
