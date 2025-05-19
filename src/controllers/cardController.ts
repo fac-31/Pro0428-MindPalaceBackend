91f4341fcd742a9d7b56570f65b573fa22e09e2f
josephpotashnik <joseph.potashnik@gmail.com>
2025-05-19 11:36:37 +0100

    get correct answers from db and comparw with the user's answers (in case of free answers, call cohere API)

--- src/controllers/cardController.ts
+++ src/controllers/cardController.ts
@@ -1,5 +1,5 @@
 import { Request, Response } from "express";
-import { generateCardsModel, insertGeneratedCards } from "../models/card";
+import { generateCardsModel, insertGeneratedCards, getFreeCardAnswerByCardId, IsUserAnswerSameAsGroundTruth, getSelectCardAnswerByCardId} from "../models/card";
 import {
     getTopicByTitle,
     getSubtopicByTopicIdAndSubtopicTitle,
@@ -49,6 +49,84 @@ export const generateCards = async (
         insertGeneratedCards(token, cards, topic, subtopic);
 
         res.status(200).json({ message: cards });
+    } catch (error) {
+        res.status(500).json({ error: "Internal Server Error" });
+    }
+};
+
+
+export const cardFreeTextAnswer = async (
+    req: Request,
+    res: Response,
+): Promise<void> => {
+    try {
+        const card_id: string = req.body.card_id;
+        const card_free_user_answer: string = req.body.user_answer;
+
+        const token = req.headers.authorization?.split(" ")[1];
+        if (!token) {
+            res.status(401).json({ error: "Unauthorized: no token provided" });
+        }
+
+        //get card from DB
+        const { data : answer, error: cardError } = await getFreeCardAnswerByCardId(
+            token,
+            card_id,
+        );
+
+        if (cardError) {
+            console.error(cardError);
+            res.status(400).json({ error: cardError.message });
+        }
+
+        //compare stored answer with user's answer
+        const similarityThreshold = 0.7;
+        const identical = IsUserAnswerSameAsGroundTruth(card_free_user_answer, answer.correct_answer,  similarityThreshold)
+
+        res.status(200).json(
+            { identical,
+              correct_answer : answer.correct_answer
+             }
+        );
+    } catch (error) {
+        res.status(500).json({ error: "Internal Server Error" });
+    }
+};
+
+
+
+export const cardSelectAnswer = async (
+    req: Request,
+    res: Response,
+): Promise<void> => {
+    try {
+        const card_id: string = req.body.card_id;
+        const card_select_user_answer: number = req.body.user_answer; // the index of the user's answer
+
+        const token = req.headers.authorization?.split(" ")[1];
+        if (!token) {
+            res.status(401).json({ error: "Unauthorized: no token provided" });
+        }
+
+        //get card from DB
+        const { data : answer, error: cardError } = await getSelectCardAnswerByCardId(
+            token,
+            card_id,
+        );
+
+        if (cardError) {
+            console.error(cardError);
+            res.status(400).json({ error: cardError.message });
+        }
+
+        //compare stored answer with user's answer
+        const identical = (card_select_user_answer == answer.correct_index);
+
+        res.status(200).json(
+            { identical,
+              correct_index : answer.correct_index
+             }
+        );
     } catch (error) {
         res.status(500).json({ error: "Internal Server Error" });
     }
--- src/routes/cardRouter.ts
+++ src/routes/cardRouter.ts
@@ -1,8 +1,11 @@
 import express from "express";
 const router = express.Router();
 
-import { generateCards } from "../controllers/cardController";
+import { generateCards , cardFreeTextAnswer, cardSelectAnswer } from "../controllers/cardController";
 
 router.post("/generateCards", generateCards);
+router.post("/cardFreeTextAnswer", cardFreeTextAnswer);
+router.post("/cardSelectAnswer", cardSelectAnswer);
+
 
 export default router;
